4c935fedfae39156811380e497da45af0aaf260e
Sundze <keyonmwesundze@mail.com>
2023-02-26 04:57:52 +0100

    creating and mounting multiple routes

--- 4_natours/app.js
+++ 4_natours/app.js
@@ -170,21 +170,19 @@ const getAllUsers = (req, res) => {
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-  .route("/api/v1/tours/:id")
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route("/").get(getAllTours).post(createTour);
 
-app.route("/api/v1/users").get(getAllUsers).post(createUser);
+tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
-app
-  .route("/api/v1/users/:id")
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route("/").get(getAllUsers).post(createUser);
+
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/tours", tourRouter);
+app.use("api/v1/users", userRouter);
 
 //Start Server
 const port = 3000;
