f3627bc4cae192e25b5e4817d68be3f3e0467bdc
Sundze <keyonmwesundze@mail.com>
2023-03-01 04:29:53 +0100

    nodejs thread pool demo

--- 2_how_node_works/event-loop.js
+++ 2_how_node_works/event-loop.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+// specifying thread pool (1-4)
+//process.env.UV_THREADPOOL_SIZE = 2;
 
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
@@ -15,6 +20,20 @@ fs.readFile("text-file.txt", () => {
 
   // a tick = an entire loop but nextTick actually means b4 the nxt loop phase
   process.nextTick(() => console.log("Process.nextTick"));
+
+  // thread pool in demo
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
 });
 
 console.log("Hello from top-level code");
