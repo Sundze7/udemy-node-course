102e54f1c2a505454a32d089d43ed6d7d9468334
Sundze <keyonmwesundze@mail.com>
2023-03-04 17:37:15 +0100

    connected app to mogoose db

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-
 const app = require("./4_natours/app");
 
-//console.log(process.env);
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("DB connection successful");
+  });
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
