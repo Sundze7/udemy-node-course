026b4c1318d24eb147061322894ea12be696789b
Sundze <keyonmwesundze@mail.com>
2023-03-08 15:13:10 +0100

    completed tour model/schema

--- 4_natours/controllers/tourController.js
+++ 4_natours/controllers/tourController.js
@@ -56,7 +56,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: "failed",
-      message: "Invalid data sent",
+      message: err,
     });
   }
 };
--- 4_natours/models/tourModel.js
+++ 4_natours/models/tourModel.js
@@ -6,14 +6,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a grp size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty level"],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQauntity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, "a tour must have a price"],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    require: [true, "A tour must have a description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  image: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
