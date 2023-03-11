79e74ec591a11115f33429b5a6a9a81ef3a8aed5
Sundze <keyonmwesundze@mail.com>
2023-03-11 06:00:29 +0100

    sorting

--- 4_natours/controllers/tourController.js
+++ 4_natours/controllers/tourController.js
@@ -5,17 +5,26 @@ exports.getAllTours = async (req, res) => {
     console.log(req.query);
 
     //BUILD QUERY
-    //1. FILTERING
+    //1.a. FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    //2. ADVANCED FILTERING
+    //1.b. ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2. SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
 
     //EXECUTE QUERY
     const tours = await query;
