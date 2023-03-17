dd582abb25f835fbf5f26f15aebeb28fd246220b
Sundze <keyonmwesundze@mail.com>
2023-03-17 03:26:09 +0100

    installing npm packages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 
 //Blocking, Synchronous way
@@ -38,6 +40,9 @@ const tempProduct = fs.readFileSync(
 //callback function
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
