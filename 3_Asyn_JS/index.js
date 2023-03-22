65189179b2bc9bc2e9f3d83074f23c52e1da14b5
Sundze <keyonmwesundze@mail.com>
2023-03-22 14:28:25 +0100

    promises - async await

--- 3_Asyn_JS/index.js
+++ 3_Asyn_JS/index.js
@@ -1,18 +1,57 @@
 const fs = require("fs");
+const { resolve } = require("path");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
-
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log("Random dog image saved to file");
-      });
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("file not found");
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("could not write to file");
+      resolve("success");
+    });
+  });
+};
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro("dog-img.txt", res.body.message);
+    console.log("Random dog image saved to file");
+  } catch (err) {
+    console.log(err);
+  }
+};
+getDogPic();
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   // .then handles successfull cases
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro("dog-img.txt", res.body.message);
+//   })
+//   .then(() => {
+//     console.log("Random dog image saved to file");
+//   })
+//   // handles errors
+//   .catch((err) => {
+//     console.log(err);
+//   });
