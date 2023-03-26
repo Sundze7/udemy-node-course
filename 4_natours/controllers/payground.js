364c6d2c085687a42a9d5691b52b189bf08824a2
Sundze <keyonmwesundze@mail.com>
2023-03-26 05:28:31 +0100

    code refactor

--- 4_natours/controllers/payground.js
+++ 4_natours/controllers/payground.js
@@ -42,3 +42,49 @@
 //   },
 // });
 // };
+
+// //BUILD QUERY
+// //1.a. FILTERING
+// const queryObj = { ...req.query };
+// const excludedFields = ["page", "sort", "limit", "fields"];
+// excludedFields.forEach((el) => delete queryObj[el]);
+
+// //1.b. ADVANCED FILTERING
+// let queryStr = JSON.stringify(queryObj);
+// queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+// console.log(JSON.parse(queryStr));
+
+// let query = Tour.find(JSON.parse(queryStr));
+
+//2. SORTING
+// if (req.query.sort) {
+//   const sortBy = req.query.sort.split(",").join(" ");
+//   console.log(sortBy);
+//   query = query.sort(sortBy);
+// } else {
+//   query = query.sort("-createdAt");
+// }
+
+//3. FIELD LIMITING
+// if (req.query.fields) {
+//   const fields = req.query.fields.split(",").join(" ");
+//   query = query.select("name difficulty price");
+//   // outputs only specified fields
+// } else {
+//   query = query.select("-__v");
+//   // the minus sign means to exclude this field in the output
+// }
+
+//4. PAGINATION
+// const page = req.query.page * 1 || 1;
+// const limit = req.query.limit * 1 || 100;
+// const skip = (page - 1) * limit;
+
+// //page=3&limit=10, 1-10: page 1, 11-20: page 2, 21-30: page 3
+// query = query.skip(skip).limit(limit);
+// // num of entry to skip b4 starting d query of limited num
+
+// if (req.query.page) {
+//   const newTours = await Tour.countDocuments();
+//   if (skip >= newTours) throw new ErrorEvent("this page does not exist");
+// }
--- 4_natours/controllers/tourController.js
+++ 4_natours/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("./../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -11,54 +12,13 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
 
-    //BUILD QUERY
-    //1.a. FILTERING
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    //1.b. ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //2. SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      console.log(sortBy);
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    //3. FIELD LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select("name difficulty price");
-      // outputs only specified fields
-    } else {
-      query = query.select("-__v");
-      // the minus sign means to exclude this field in the output
-    }
-
-    //4. PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    //page=3&limit=10, 1-10: page 1, 11-20: page 2, 21-30: page 3
-    query = query.skip(skip).limit(limit);
-    // num of entry to skip b4 starting d query of limited num
-
-    if (req.query.page) {
-      const newTours = await Tour.countDocuments();
-      if (skip >= newTours) throw new ErrorEvent("this page does not exist");
-    }
-
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+    const tours = await features.query;
 
     // const tours = await Tour.find()
     //   .where("duration")
