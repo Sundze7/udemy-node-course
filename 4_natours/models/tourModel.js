2e9fe22af6bdb28689a8af08ec7a4550eacfaa86
Sundze <keyonmwesundze@mail.com>
2023-03-30 15:20:43 +0100

    document middlewares

--- 4_natours/models/tourModel.js
+++ 4_natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -62,6 +64,23 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+//DOC MIDDLEWARE: runs b4 .save() and .create(). Doesn't work on .insertMany()
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  //this points to the currently being saved doc
+  next();
+});
+
+// tourSchema.pre("save", function (next) {
+//   console.log("Will save doc...");
+//   next();
+// });
+
+// tourSchema.post("save", function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
