9273a01f64379f5b3fb09368440d49d6586898a2
Sundze <keyonmwesundze@mail.com>
2023-04-05 11:40:33 +0100

    unhandled routes

--- 4_natours/app.js
+++ 4_natours/app.js
@@ -16,24 +16,9 @@ app.use(express.json()); // body parser middleware
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("hello from the middleware");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: "Hello from the server side", app: "Natours" });
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("You can post to this endpoint...");
-// });
 
 // End-points or Routes
 
@@ -45,5 +30,12 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "failed",
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
