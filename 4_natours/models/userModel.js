528760d8a96f338a75412198d90ba16c86e040c7
Sundze <keyonmwesundze@mail.com>
2023-04-11 15:08:50 +0100

    created user model, added validators

---
+++ 4_natours/models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bycrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "please provide a passsword"],
+    minlength: [8, "password must be at least 8 characters"],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      //Rem this only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  //only runs if password was modified
+  if (!this.isModified("password")) return next();
+
+  //hash password with cost 12
+  this.password = await bycrypt.hash(this.password, 12);
+
+  //delete passwordConfirm field
+  this.confirmPassword = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- 4_natours/routes/userRoutes.js
+++ 4_natours/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const userControllers = require("./../controllers/userControllers");
+const authController = require("../controllers/authController.js");
 
 const router = express.Router();
+
+router.post("/signup", authController.signup);
 
 router
   .route("/")
