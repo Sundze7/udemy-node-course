a5c1e1f91ff0dc84a95edd585f944c71f172d368
Sundze <keyonmwesundze@mail.com>
2023-04-12 14:44:13 +0100

    user login and jwt auth

--- 4_natours/controllers/userControllers.js
+++ 4_natours/controllers/userControllers.js
@@ -1,3 +1,18 @@
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
@@ -20,13 +35,6 @@ exports.updateUser = (req, res) => {
 };
 
 exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "this route is not yet defined",
-  });
-};
-
-exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: "error",
     message: "this route is not yet defined",
--- 4_natours/models/userModel.js
+++ 4_natours/models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "please provide a passsword"],
     minlength: [8, "password must be at least 8 characters"],
+    select: false,
   },
   confirmPassword: {
     type: String,
@@ -44,6 +45,13 @@ userSchema.pre("save", async function (next) {
   this.confirmPassword = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bycrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
