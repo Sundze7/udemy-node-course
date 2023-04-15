fe094952ab11db0666a326100a56128aad4ee279
Sundze <keyonmwesundze@mail.com>
2023-04-15 15:54:17 +0100

    added cookie, rate limiting, data sanitization and more...

--- 4_natours/app.js
+++ 4_natours/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,15 +13,52 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// middlewares
+// Global Middlewares
+// set security http headers
+app.use(helmet());
+
+//dev logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json()); // body parser middleware
+// limit req from same api: to prevent brute force and DOS attacks
+const limiter = rateLimit({
+  //allow only 100 reqs per hr
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from IP, please try again in an hour",
+});
+app.use("/api", limiter); // applies only to routes stating with /api
+
+// body parser: reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization aginst XSS
+app.use(xss());
+
+//prevent param pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
