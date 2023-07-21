141a7a172c44ba0d21d614c26cc3a7a0088f050c
Sundze <keyonmwesundze@mail.com>
2023-07-22 00:34:13 +0100

    hosting

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose
     console.log("DB connection successful");
   });
 
-const port = 3000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
